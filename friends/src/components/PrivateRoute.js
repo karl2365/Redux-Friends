d2269f96ceb09b5db7e9a630a01864a76e613a49
Karl Beksa <karlbeksa@gmail.com>
2019-06-20 19:51:34 -0400

    dynamic private route

--- friends/src/components/PrivateRoute.js
+++ friends/src/components/PrivateRoute.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Route, Redirect} from 'react-router-dom';
-import FriendsListView from './FriendsListView';
 
 
-const PrivateRoute = ({...rest}) => {
+const PrivateRoute = ({component: Component, ...rest}) => {
     return (
         <Route {...rest}
         render={props => localStorage.getItem('token') ?
-        (<FriendsListView {...props} />) :
+        (<Component {...props} />) :
         ( <Redirect to='/api/login' />
         
         )} />
