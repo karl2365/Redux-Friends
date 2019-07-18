11bb90e2e542be1a6bf3a726b5e843b6cdcf58db
Karl Beksa <karlbeksa@gmail.com>
2019-07-18 19:09:13 -0400

    with add friend

--- friends/src/components/friends.js
+++ friends/src/components/friends.js
@@ -4,9 +4,46 @@ import './component.css';
 
 
 
-
 function Friends({ history }) {
 const [friendsList, setFriendsList] = useState([]);
+const [newFriend, setNewFriend] = useState({});
+
+const handleChanges = e => {
+    setNewFriend({...newFriend, [e.target.name]: e.target.value})
+}
+
+const handleSubmit = e => {
+    e.preventDefault()
+    console.log('new friend', newFriend)
+ 
+    const token = localStorage.getItem("token");
+    const url =
+      "http://localhost:5000/api/friends";
+
+    if (token) {
+        console.log('in conditional')
+      axios
+        .post(url, newFriend,  {
+          headers: {
+            Authorization: `${token}` 
+          }
+        })
+        .then(response => {
+            console.log('response', response)
+            history.push('/friends')
+        })
+        .catch(e => {
+          console.log(e.response);
+         
+        });
+    }
+
+
+}
+
+
+
+
   useEffect(() => {
     const token = localStorage.getItem("token");
     const url =
@@ -16,7 +53,7 @@ const [friendsList, setFriendsList] = useState([]);
       axios
         .get(url, {
           headers: {
-            Authorization: `${token}` // for the project just set it to token
+            Authorization: `${token}` 
           }
         })
         .then(response => {
@@ -43,6 +80,15 @@ const [friendsList, setFriendsList] = useState([]);
         <p className="friendAge">Age: {friend.age}</p>
         <p className="friendEmail">Email: {friend.email}</p>
       </div>)}
+
+        <form onSubmit={handleSubmit}>
+            <input type='text' name='name' placeholder='name' onChange={handleChanges} value={newFriend.name} />
+            <input type='text' name='age' placeholder='age'  onChange={handleChanges} value={newFriend.age}  />
+            <input type='email' name='email' placeholder='email'  onChange={handleChanges}  value={newFriend.email} />
+            <button type='submit'>submit</button>
+
+        </form>
+
       <button
         className="btn logoutButton"
         onClick={() => {
